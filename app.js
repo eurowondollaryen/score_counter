0d7156865f1a3d89da85e5f283fa9022596cc50a
eurowondollaryen <sehoakasayho@naver,com>
2018-09-19 00:50:27 +0900

    initial commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/', function(req, res) {
 			console.log(err);
 		}
 		else {
-			res.writeHead(200, {'Content-type' : 'text/html'});
+			res.writeHead(200, {'Content-Type' : 'text/html'});
 			res.end(data);
 		}
 	});
