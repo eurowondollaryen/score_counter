8bb5fc07e26bd87da98becafeaeba25f64a57f34
eurowondollaryen <sehoakasayho@naver,com>
2018-09-19 00:53:15 +0900

    initial commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 const app = express();
 var fs = require('fs');
 var path = require('path');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, function() {
